ea8001edbd860eb05ea4d02c4be94006a39a7db3
wishiz <eugeniavoloshina@gmail.com>
2022-08-30 11:49:20 -0600

    init repository

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import Component from "./components/Component/Component.jsx";
+import "./App.css";
+
+const App = () => {
+  return (
+    <main id="app-container">
+      <Component />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Component/Component.jsx
@@ -0,0 +1,8 @@
+import React from "react";
+import "./Component.css";
+
+const Component = () => {
+  return <h3 className="greeting">Hello World</h3>;
+};
+
+export default Component;
